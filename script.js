5fa8f74186b4c0e5577fce4e27858dc3d587a4b6
Christine <christine.nguyen0909@gmail.com>
2025-08-16 12:11:41 -0700

    writing first function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function getComputerChoice(){
+    const randomChoice = Math.random();
+  
+    if (randomChoice < 0.33) 
+    {
+      return "rock";
+    } 
+    else if (randomChoice < 0.66) 
+    {
+      return "paper";
+    } 
+    else 
+    {
+      return "scissors";
+    }
+  }
