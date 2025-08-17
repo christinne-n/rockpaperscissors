3008b9d75877f1c446350a64c271bd2a0672db6b
Christine <christine.nguyen0909@gmail.com>
2025-08-17 11:04:05 -0700

    adding playround function to keep score

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     const randomChoice = Math.random();
   
@@ -20,4 +23,24 @@ function getComputerChoice(){
     if(playerAnswer === "p") return "paper";
     if(playerAnswer === "s") return "scissors";
   }
+  function playRound(humanChoice, computerChoice) {
+    console.log("You chose ${humanChoice}");
+    console.log("Computer chose ${computerChoice}");
+    
+    if(humanChoice === computerChoice){
+      console.log("It is a tie!")
+    }
+    else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper") ) {
+        console.log("You win!");
+        humanScore++;
+      }
+    else{
+      console.log("You lose!");
+      computerScore++;
+    }
+  }
+  
   
